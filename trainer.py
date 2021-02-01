f700185d3f845a74ad7c35aa8b6521153cf02098
Buhua-Liu <55881942+Buhua-Liu@users.noreply.github.com>
2021-02-01 15:04:07 +0800

    Refactor

--- trainer.py
+++ trainer.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-import shutil
 import time
+from datetime import datetime
 
 import torch
 import torch.nn as nn
@@ -15,63 +15,62 @@ from torch.utils.tensorboard import SummaryWriter
 
 import resnet
 
-model_names = sorted(name for name in resnet.__dict__
+model_names = [name for name in resnet.__dict__
     if name.islower() and not name.startswith("__")
                      and name.startswith("resnet")
-                     and callable(resnet.__dict__[name]))
-
-# print(model_names)
+                     and callable(resnet.__dict__[name])]
 
 parser = argparse.ArgumentParser(description='Proper ResNets for CIFAR10 in pytorch')
 parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet32',
                     choices=model_names,
                     help='model architecture: ' + ' | '.join(model_names) +
                     ' (default: resnet32)')
-parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+parser.add_argument('--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=1000, type=int, metavar='N',
-                    help='number of total epochs to run')
+parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                    help='number of total epochs to run (default: 200)')
+parser.add_argument('--resume', default=None, type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: None)')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=128, type=int,
                     metavar='N', help='mini-batch size (default: 128)')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                    metavar='LR', help='initial learning rate')
+                    metavar='LR', help='initial learning rate (default: 0.1)')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
+                    help='momentum (default: 0.9)')
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
-parser.add_argument('--print-freq', '-p', default=50, type=int,
+parser.add_argument('-f', '--print-freq', default=50, type=int,
                     metavar='N', help='print frequency (default: 50)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
-                    help='evaluate model on validation set')
+                    help='whether to evaluate model on validation set')
 parser.add_argument('--pretrained', dest='pretrained', action='store_true',
-                    help='use pre-trained model')
+                    help='whether to use pre-trained model')
 parser.add_argument('--amp', dest='amp', action='store_true',
-                    help='use automatic mixed precision (AMP) ')
-parser.add_argument('--save-dir', dest='save_dir',
-                    help='The directory used to save the trained models',
-                    default='save_temp', type=str)
-parser.add_argument('--save-every', dest='save_every',
-                    help='Saves checkpoints at every specified number of epochs',
-                    type=int, default=10)
-parser.add_argument('--gpu-id', default=0, type=int, help=' GPU index')
+                    help='whether to use automatic mixed precision (AMP) ')
+parser.add_argument('--save-dir', dest='save_dir', default='save_temp', type=str,
+                    help='directory used to save the trained models')
+parser.add_argument('--save-every', dest='save_every', default=10, type=int,
+                    help='save checkpoints every SAVE_EVERY epochs (default: 10)')
+parser.add_argument('--gpu-id', default=0, type=int, help='GPU index (default: 0)')
+
 best_prec1 = 0
 
 
 def main():
     global args, best_prec1, writer
     args = parser.parse_args()
+    writer = SummaryWriter('runs/' + args.arch)
+    print(args)
 
 
-    device = torch.device(f'cuda:{ars.gpu_id}' if torch.cuda.is_available() else "cpu")
+    device = torch.device(f'cuda:{args.gpu_id}' if torch.cuda.is_available() else "cpu")
     
     # Check the save_dir exists or not
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
-    
+
     model = resnet.__dict__[args.arch]()
     model.to(device)
 
@@ -84,7 +83,7 @@ def main():
             best_prec1 = checkpoint['best_prec1']
             model.load_state_dict(checkpoint['state_dict'])
             print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.evaluate, checkpoint['epoch']))
+                  .format(args.resume, checkpoint['epoch']))
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
@@ -112,9 +111,10 @@ def main():
         num_workers=args.workers, pin_memory=True)
 
     # define loss function (criterion) and optimizer
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss().to(device)
 
     if args.amp:
+        global scaler
         scaler = torch.cuda.amp.GradScaler()
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
@@ -135,35 +135,51 @@ def main():
         validate(val_loader, model, criterion)
         return
 
-    writer = SummaryWriter('runs/' + args.arch)
-
+    print(f"Start training at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    epoch_time = AverageMeter()
+    train_time = AverageMeter()
+    val_time = AverageMeter()
     for epoch in range(args.start_epoch, args.epochs):
 
         # train for one epoch
-        print('current lr {:.5e}'.format(optimizer.param_groups[0]['lr']))
+        start=time.time()
+        print('Current learning rate: {:.5e}'.format(optimizer.param_groups[0]['lr']))
         writer.add_scalar('learning rate', optimizer.param_groups[0]['lr'], epoch)
+        tmp = time.time()
         train(train_loader, model, criterion, optimizer, epoch)
+        train_time.update(time.time()-tmp)
+        print(f'Epoch {epoch} training time: {epoch_time.val} (average: {train_time.avg})')
         lr_scheduler.step()
 
         # evaluate on validation set
+        tmp = time.time()
         prec1 = validate(val_loader, model, criterion, epoch)
+        val_time.update(time.time()-tmp)
+        print(f'Epoch {epoch} validation time: {epoch_time.val} (average: {val_time.avg})')
 
         # remember best prec@1 and save checkpoint
         is_best = prec1 > best_prec1
         best_prec1 = max(prec1, best_prec1)
-
-        if epoch > 0 and epoch % args.save_every == 0:
-            print(f"Saving checkpoint at epoch {epoch}...")
-            save_checkpoint({
+        if is_best:
+            torch.save({
                 'epoch': epoch + 1,
                 'state_dict': model.state_dict(),
                 'best_prec1': best_prec1,
-            }, is_best, filename=os.path.join(args.save_dir, f'checkpoint_{epoch}.th'))
-            
-        save_checkpoint({
+            }, os.path.join(args.save_dir, f'best_checkpoint_{epoch+1}.th'))
+
+        # save checkpoint regularly
+        if epoch > 0 and (epoch + 1) % args.save_every == 0:
+            print(f"Saving checkpoint at epoch {epoch}...")
+            torch.save({
+                'epoch': epoch + 1,
                 'state_dict': model.state_dict(),
                 'best_prec1': best_prec1,
-            }, is_best, filename=os.path.join(args.save_dir, 'last_epoch.th'))
+            }, os.path.join(args.save_dir, f'checkpoint_{epoch+1}.th'))
+
+
+        epoch_time.update(time.time()-start)
+        print(f'Epoch {epoch} validation time: {epoch_time.val} (average: {val_time.avg})')
+        print(f"Current time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 
 def train(train_loader, model, criterion, optimizer, epoch):
@@ -186,7 +202,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         target = target.to(device)
         input_var = image.to(device)
-        target_var = target.to(device)
+        target_var = target
         
         optimizer.zero_grad()
 
@@ -219,10 +235,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
         prec1 = accuracy(output.data, target)[0]
         losses.update(loss.item(), input.size(0))
         top1.update(prec1.item(), input.size(0))
-        
 
-        writer.add_scalar('training loss', losses.val, epoch)
-        writer.add_scalar('training accuracy', top1.val, epoch)
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -236,6 +249,9 @@ def train(train_loader, model, criterion, optimizer, epoch):
                   'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
                       epoch, i, len(train_loader), batch_time=batch_time,
                       data_time=data_time, loss=losses, top1=top1))
+
+    writer.add_scalar('training loss', losses.avg, epoch)
+    writer.add_scalar('training accuracy', top1.avg, epoch)
 
 
 def validate(val_loader, model, criterion, epoch=None):
@@ -254,7 +270,7 @@ def validate(val_loader, model, criterion, epoch=None):
         for i, (image, target) in enumerate(val_loader):
             target = target.to(device)
             input_var = image.to(device)
-            target_var = target.to(device)
+            target_var = target
 
             # if args.amp:
             #     input_var = input_var.half()
@@ -271,8 +287,6 @@ def validate(val_loader, model, criterion, epoch=None):
             losses.update(loss.item(), input.size(0))
             top1.update(prec1.item(), input.size(0))
 
-            writer.add_scalar('test loss', losses.val, epoch)
-            writer.add_scalar('test accuracy', top1.val, epoch)
 
             # measure elapsed time
             batch_time.update(time.time() - end)
@@ -288,16 +302,10 @@ def validate(val_loader, model, criterion, epoch=None):
 
     print(' * Prec@1 {top1.avg:.3f}'
           .format(top1=top1))
+    writer.add_scalar('test loss', losses.avg, epoch)
+    writer.add_scalar('test accuracy', top1.avg, epoch)
 
     return top1.avg
-
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    """
-    Save the training model
-    """
-    torch.save(state, filename)
-    if is_best:
-        torch.save(state, os.path.join(args.save_dir, 'best_checkpoint.th'))
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
